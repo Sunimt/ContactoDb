8f2b2dfa4c84955f0cc5518d1c64c111d6423764
cisco01 <cisco01@CISCO1>
2023-10-23 12:00:02 -0700

    Agregar archivos de proyecto.

---
+++ ContactoDb/Data/Conexion.cs
@@ -0,0 +1,20 @@
+﻿namespace ContactoDb.Data
+{
+    public class Conexion
+    {
+        private string cadenaSql = string.Empty;
+
+        public Conexion()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(
+                Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            cadenaSql = builder.GetSection("ConnectionStrings:cadenaSql").Value;
+        }
+
+        public string getCadenaSql()
+        {
+            return cadenaSql;
+        }
+
+    }
+}
---
+++ ContactoDb/Data/ContactoDatos.cs
@@ -0,0 +1,150 @@
+﻿using ContactoDb.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ContactoDb.Data
+{
+    public class ContactoDatos
+    {
+        public List<ContactoModel> ListarContacto()
+        {
+            List<ContactoModel> lista = new List<ContactoModel>();
+            var cn = new Conexion();
+            using (var conexion = new SqlConnection(cn.getCadenaSql()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_ListarContacto", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        lista.Add(new ContactoModel()
+                        {
+                            IdContacto = Convert.ToInt32(dr["IdContacto"]),
+                            Nombre = dr["Nombre"].ToString(),
+                            Telefono = dr["Telefono"].ToString(),
+                            Correo = dr["Correo"].ToString(),
+                            Clave = dr["Clave"].ToString()
+                        });
+                    }
+                }
+            }
+
+            return lista;
+        }
+
+        public ContactoModel ObtenerContacto(int IdContacto)
+        {
+            var oContacto = new ContactoModel();
+            var cn = new Conexion();
+            using (var conexion = new SqlConnection(cn.getCadenaSql()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_Obtener", conexion);
+                cmd.Parameters.AddWithValue("IdContacto", IdContacto);
+                cmd.CommandType = CommandType.StoredProcedure;
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oContacto.IdContacto = Convert.ToInt32(dr["IdContacto"]);
+                        oContacto.Nombre = dr["Nombre"].ToString();
+                        oContacto.Telefono = dr["Telefono"].ToString();
+                        oContacto.Correo = dr["Correo"].ToString();
+                        oContacto.Clave = dr["Clave"].ToString();
+                    }
+                }
+
+                return oContacto;
+            }
+        }
+
+        public bool GuardarContacto(ContactoModel model)
+        {
+            bool respuesta;
+            try 
+            {
+                var cn = new Conexion();
+                using (var conexion = new SqlConnection(cn.getCadenaSql()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_GuardarContacto", conexion);
+
+                    cmd.Parameters.AddWithValue("Nombre", model.Nombre);
+                    cmd.Parameters.AddWithValue("Telefono", model.Telefono);
+                    cmd.Parameters.AddWithValue("Correo", model.Correo);
+                    cmd.Parameters.AddWithValue("Clave", model.Clave);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.ExecuteNonQuery();
+
+                }
+                respuesta = true;
+            }
+            catch(Exception e)
+            {
+                string error = e.Message;
+                respuesta=false;
+            }
+            return respuesta;
+        }
+
+        public bool EditarContacto(ContactoModel model)
+        {
+            bool respuesta;
+            try
+            {
+                var cn = new Conexion();
+                using (var conexion = new SqlConnection(cn.getCadenaSql()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_EditarContacto", conexion);
+
+                    cmd.Parameters.AddWithValue("IdContacto", model.IdContacto);
+                    cmd.Parameters.AddWithValue("Nombre", model.Nombre);
+                    cmd.Parameters.AddWithValue("Telefono", model.Telefono);
+                    cmd.Parameters.AddWithValue("Correo", model.Correo);
+                    cmd.Parameters.AddWithValue("Clave", model.Clave);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.ExecuteNonQuery();
+
+                }
+                respuesta = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                respuesta = false;
+            }
+            return respuesta;
+        }
+
+        public bool EliminarContacto(int IdContacto)
+        {
+            bool respuesta;
+            try
+            {
+                var cn = new Conexion();
+                using (var conexion = new SqlConnection(cn.getCadenaSql()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Eliminar", conexion);
+
+                    cmd.Parameters.AddWithValue("IdContacto", IdContacto);
+                    cmd.CommandType = CommandType.StoredProcedure;  
+                    cmd.ExecuteNonQuery();
+
+                }
+                respuesta = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                respuesta = false;
+            }
+            return respuesta;
+        }
+    }
+}
